9a69a0791deed97d84f7bb35bc48c3c2da8b46c3
Om Patel <ompatel10312@gmail.com>
2026-07-08 18:06:10 +0530

    Hello Friends

--- app/src/main/java/com/example/applicationtemp/LoginActivity.kt
+++ app/src/main/java/com/example/applicationtemp/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.applicationtemp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+    val TAG = "LoginActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,25 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        display(msg = "onCreate method is called.")
+    }
+
+    override fun onStart() {
+        display(msg = "onStart method is called.")
+        super.onStart()
+    }
+
+    override fun onPause() {
+        display(msg = "onPause method is called.")
+        super.onPause()
+    }
+
+    override fun onResume() {
+        display(msg = "OnResume method is called.")
+        super.onResume()
+    }
+    fun display(msg:String){
+        Log.i(TAG,msg)
+        Toast.makeText(this, "msg", Toast.LENGTH_SHORT).show()
     }
 }
